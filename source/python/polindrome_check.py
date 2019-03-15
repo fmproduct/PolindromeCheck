b0e3abc8d901e857ba65f9d7098a52bcf92405c0
FMProducts <44195852+FMProducts@users.noreply.github.com>
2019-03-15 14:58:16 +0300

    Add files via upload

---
+++ source/python/polindrome_check.py
@@ -0,0 +1,28 @@
+
+def reverse(data):
+	s = str(data)
+	return int(s[::-1]);
+
+def isPolindrome(d):
+	return reverse(d) == d
+
+i = 0
+x = 10
+y = 9000
+j = int(input('input number:	'))
+print('------------------------------------------')
+print('original:	'+str(j))
+
+while not isPolindrome(j):
+	# iteration limit
+	if i > 1000:
+		break
+	i = i + 1
+	j = j + reverse(j)
+
+if i < 1000:
+	print('iterations:	'+str(i))
+else:
+	print("iterations:	 1000+")
+print('output:		'+str(j))
+i = 0
